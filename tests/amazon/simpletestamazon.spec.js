7c896e1232118ac2c3bdbe5fc396801c0242371c
PratheepRaj-009 <pratheepkumar561@gmail.com>
2026-09-23 15:22:45 +0530

    spelling mistake

--- tests/amazon/simpletestamazon.spec.js
+++ tests/amazon/simpletestamazon.spec.js
@@ -1,6 +1,6 @@
 import {test} from"@playwright/test"
 
-test("helloGithub @reggression @smoke",async({page})=>
+test("helloGithub @regression @smoke",async({page})=>
 {
     await page.goto("https://www.amazon.in/")
     console.log("direct changes from GitHub globa;l repository")
