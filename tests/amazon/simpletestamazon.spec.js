5144df5406af80172d456b7205a9381531015f8d
PratheepRaj-009 <pratheepkumar561@gmail.com>
2026-09-23 15:01:55 +0530

    removed all report files and Added Jenkins regression pipeline in package.json

--- tests/amazon/simpletestamazon.spec.js
+++ tests/amazon/simpletestamazon.spec.js
@@ -1,6 +1,6 @@
 import {test} from"@playwright/test"
 
-test("helloGithub",async({page})=>
+test("helloGithub @reggression @smoke",async({page})=>
 {
     await page.goto("https://www.amazon.in/")
     console.log("direct changes from GitHub globa;l repository")
